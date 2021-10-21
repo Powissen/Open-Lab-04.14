f8c6c2572398053688415ed41a0d39ca937ca393
Lukáš Lokša <lukasloksa2@gmail.com>
2021-10-21 19:00:30 +0200

    Function that puts words from string array into a frame

--- Open-Lab-04.14/FramePrinter.cs
+++ Open-Lab-04.14/FramePrinter.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+
 
 namespace Open_Lab_04._14
 {
@@ -6,7 +8,20 @@ namespace Open_Lab_04._14
     {
         public void Print(string[] strings)
         {
-            throw new NotImplementedException();
+            int longest = strings.Max(w => w.Length);
+            string stars = new String('*', longest + 4);
+            Console.WriteLine(stars);
+
+            foreach (string word in strings)
+            {
+                int wordLenght = word.Length;
+
+                int SpaceChar = longest - word.Length; 
+                string space = new String(' ', SpaceChar);
+
+                Console.WriteLine("* " + word + space + " *");
+            }
+            Console.WriteLine(stars);
         }
     }
 }
